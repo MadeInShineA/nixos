25e9672e3893fdf6c2c5cf0ee0125877ba02f83b
Olivier Amacker <olivier.amacker@netplus.ch>
2026-02-13 12:30:40 +0100

    Initial commit

---
+++ home.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "madeinshinea";
+  home.homeDirectory = "/home/madeinshinea";
+
+  # Import files from the current configuration directory into the Nix store,
+  # and create symbolic links pointing to those store files in the Home directory.
+
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # Import the scripts directory into the Nix store,
+  # and recursively generate symbolic links in the Home directory pointing to the files in the store.
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    fastfetch
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "Olivier Amacker";
+    userEmail = "olivier.amacker@netplus.ch";
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+}
