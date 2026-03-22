58ba2b2e45b95faae511c20f64c0b12c08ec41e3
Olivier Amacker <olivier.amacker@netplus.ch>
2026-03-22 17:11:24 +0100

    Replaced power daemon by tlp

--- common/configuration.nix
+++ common/configuration.nix
@@ -123,8 +123,6 @@
     HandlePowerKey = "ignore";
   };
 
-  services.power-profiles-daemon.enable = true;
-
   # Enable tailscale
   services.tailscale.enable = true;
 
@@ -141,6 +139,32 @@
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];
+
+  # Power management
+  # services.power-profiles-daemon.enable = true;
+
+  services.thermald.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+
+    };
+  };
 
   system.stateVersion = "25.11";
 }
